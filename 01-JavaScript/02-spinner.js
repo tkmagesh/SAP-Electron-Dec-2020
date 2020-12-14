95f9610aa54165b06e125a4988af00b0bd4299ab
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-14 18:13:03 +0530

    constructor functions

--- 01-JavaScript/02-spinner.js
+++ 01-JavaScript/02-spinner.js
@@ -79,3 +79,19 @@ function Spinner(){
         return --counter;
     };
 }
+
+
+//Using prototypes
+
+function Spinner(){ 
+    this.$$counter = 0;
+}
+
+Spinner.prototype['up'] = function(){
+    return ++this.$$counter;
+};
+
+Spinner.prototype['down'] = function(){
+    return --this.$$counter;
+};
+
