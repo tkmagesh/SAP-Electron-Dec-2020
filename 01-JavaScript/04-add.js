baa798f9457e2a48fee76580d8d0a468ae29c4e4
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-14 19:17:18 +0530

    async demos - initial

--- 01-JavaScript/04-add.js
+++ 01-JavaScript/04-add.js
@@ -6,3 +6,14 @@ function add(){
     }
     return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add(Array.prototype.slice.call(arguments, 1));
 }
+
+//ES6 Version of the above
+function add(...args){
+    function parseArg(n){
+        if (Array.isArray(n)) return add(...n);
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return args.length <= 1 ? parseArg(args[0]) : parseArg(args[0]) + add(args.slice(1));
+}
+
